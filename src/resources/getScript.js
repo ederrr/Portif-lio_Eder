d94977aacbc20a31e558a9886abb804181b5625f
Eder RR <ederrosate@yahoo.com.br>
2018-11-01 18:07:46 -0300

    correcao celular contato

--- src/resources/getScript.js
+++ src/resources/getScript.js
@@ -4,7 +4,7 @@ export const portugueseCoursesOnline = ['Cursos Online','Carga Horária','Térmi
 export const portugueseCoursesLanguage = ['Idiomas']
 export const portugueseCoursesTechnical = ['Cursos','a']
 export const portugueseCoursesGrade = ['Formação Acadêmica','a']
-export const portugueseContact = ['Entre em Contato','email@gmail.com','Digite sua Mensagem...','Enviar Mensagem', "Outros Contatos", "Celular", "Email"]
+export const portugueseContact = ['Entre em Contato','email@gmail.com','Digite sua Mensagem...','Enviar Mensagem', "Outros Contatos", "Whatsapp", "Email"]
 
 export const englishNavibar = ['Projects','Galery','Skills','Education','Contact']
 export const englishSkills = ['Programming','Web','Mobile','Design','Modeling']
@@ -12,7 +12,7 @@ export const englishCoursesOnline = ['Online Courses','Time','Finish','Site Link
 export const englishCoursesLanguage = ['Languages']
 export const englishCoursesTechnical = ['Courses','to']
 export const englishCoursesGrade = ['University Degree','to']
-export const englishContact = ['Contact me','email@gmail.com','Type your Text ...', 'Send Message', "Others Contacts", "Mobile", "Email"]
+export const englishContact = ['Contact me','email@gmail.com','Type your Text ...', 'Send Message', "Others Contacts", "Whatsapp", "Email"]
 
 export const spanishNavibar = ['Proyectos','Galería','Habilidades','Graduación','Contacto']
 export const spanishSkills = ['Programación','Web','Mobile','Diseño','Modelaje']
@@ -20,7 +20,7 @@ export const spanishCoursesOnline = ['Cursos Online','Carga Horária','Término'
 export const spanishCoursesLanguage = ['Idiomas']
 export const spanishCoursesTechnical = ['Cursos','a']
 export const spanishCoursesGrade = ['Formação Acadêmica','a']
-export const spanishContact = ['Entre em Contato','email@gmail.com','Digite sua Mensagem...','Enviar Mensagem', "Outros Contatos", "Celular", "Email"]
+export const spanishContact = ['Entre em Contato','email@gmail.com','Digite sua Mensagem...','Enviar Mensagem', "Outros Contatos", "Whatsapp", "Email"]
 
 export const navibarScript = [englishNavibar ,portugueseNavibar ,spanishNavibar ];
 export const skillsScript = [englishSkills ,portugueseSkills ,spanishSkills];
