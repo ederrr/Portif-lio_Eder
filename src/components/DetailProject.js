e5b47f82d5729c4273e472cf6788d7c2eae2c7fd
Eder RR <ederrosate@yahoo.com.br>
2019-02-04 00:22:44 -0200

    fix margin in detailproject

--- src/components/DetailProject.js
+++ src/components/DetailProject.js
@@ -37,14 +37,14 @@ class DetailProject extends Component {
 
 	render() {
 		return (
-			<div className={`row col-md-10 col-12 offset-md-1 offset-0`}>
-        		<div className="rounded-top col col-12 p-1" style={{backgroundColor: '#07454F'}}>
+			<div className={`row col-md-10 col-12 offset-md-1 offset-0 mx-auto p-auto`}>
+        		<div className="rounded-top col col-12 p-1 mb-3" style={{backgroundColor: '#07454F'}}>
 					<p className="h6 text-white" >{this.state.name}</p>
         		</div>
-				<div className="col col-md-5 col-12 my-4">
+				<div className="col col-md-5 col-12 mb-md-0 mb-2">
 					<img className= "img-fluid w-100 rounded" src={this.state.image} alt="project"></img>
 				</div>
-				<div className="col col-md-7 col-12 my-4">
+				<div className="col col-md-7 col-12">
 					<p className="" ><label></label>{this.state.description}</p>
 					<p className="text" ><label className="h6 text-secondary">{script[this.props.script][0]}:</label> {this.state.language}</p>
 					<p className="h6 text-secondary" >{script[this.props.script][1]}: <Moment format={script[this.props.script][3]} className="small" >{this.state.created}</Moment></p>
